604f195b06046aaf205bfedc1d58920865b5fa01
mennadiaa <menna2400898@miuegypt.edu.eg>
2026-06-04 02:47:06 +0300

    added passwordhashing in RegisterController.js

--- myapp/controllers/RegisterController.js
+++ myapp/controllers/RegisterController.js
@@ -1,29 +1,27 @@
 const User = require("../models/RegisterModel");
+const bcrypt = require("bcryptjs"); // add this line
 
 exports.registerUser = async (req, res) => {
     try {
-
         const { name, email, password } = req.body;
 
         if (!name || !email || !password) {
-            return res.status(400).json({
-                message: "All fields are required"
-            });
+            return res.status(400).json({ message: "All fields are required" });
         }
 
-        const existingUser =
-            await User.findOne({ email });
-
+        const existingUser = await User.findOne({ email });
         if (existingUser) {
-            return res.status(400).json({
-                message: "Email already exists"
-            });
+            return res.status(400).json({ message: "Email already exists" });
         }
+
+        // hash the password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
         const user = await User.create({
             name,
             email,
-            password
+            password: hashedPassword // save hashed, not plain text
         });
 
         res.status(201).json({
@@ -32,10 +30,6 @@ exports.registerUser = async (req, res) => {
         });
 
     } catch (err) {
-
-        res.status(500).json({
-            message: err.message
-        });
-
+        res.status(500).json({ message: err.message });
     }
 };
