907acc907a6e4b5fd9e889567b81337df0a91b06
retaj2401522 <retaj2401522@miuegypt.edu.eg>
2026-06-04 02:23:59 +0300

    Added search function& a search bar

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -34,4 +34,59 @@ async function renderEvents() {
   `).join("");
 }
 
-document.addEventListener("DOMContentLoaded", renderEvents);
+const container = document.getElementById("eventsContainer");
+
+async function searchEvents() {
+  const query = document.getElementById("searchInput").value;
+
+  try {
+    const res = await fetch(
+      `http://localhost:3000/api/events?search=${query}`
+    );
+
+    const events = await res.json();
+
+    render(events);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function render(events) {
+  if (!container) return;
+
+  if (!events.length) {
+    container.innerHTML = "<p>No events found</p>";
+    return;
+  }
+
+  container.innerHTML = events.map(event => `
+    <div class="event-card">
+      <h3>${event.title}</h3>
+      <p>${event.category}</p>
+      <p>${event.location}</p>
+      <p>${event.date} | ${event.time}</p>
+      <p>${event.price} EGP</p>
+    </div>
+  `).join("");
+}
+
+async function filterCategory(category) {
+  try {
+    const res = await fetch(
+      `http://localhost:3000/api/events?category=${category}`
+    );
+
+    const events = await res.json();
+    render(events);
+
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const res = await fetch("http://localhost:3000/api/events");
+  const events = await res.json();
+  render(events);
+});
