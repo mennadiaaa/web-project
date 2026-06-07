6ab80b46bc59c33ff0b8b07c70f434bd894c0eb1
mennadiaa <menna2400898@miuegypt.edu.eg>
2026-06-08 00:39:16 +0300

    admin authorization
    user must login with specified admin emails to be redirected to admin page

--- myapp/models/user.js
+++ myapp/models/user.js
@@ -5,16 +5,19 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-
     email: {
         type: String,
         required: true,
         unique: true
     },
-
     password: {
         type: String,
         required: true
+    },
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user"
     }
 });
 
