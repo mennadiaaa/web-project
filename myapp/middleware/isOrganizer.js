da6d222d463edc1fbc386998d07e84053bbca55f
mennadiaa <menna2400898@miuegypt.edu.eg>
2026-06-08 22:41:47 +0300

    fix: lowercase user model path in isOrganizer

--- myapp/middleware/isOrganizer.js
+++ myapp/middleware/isOrganizer.js
@@ -1,5 +1,4 @@
-const User = require("../models/User");
-
+const User = require("../models/user");
 module.exports = async (req, res, next) => {
     try {
         const user = await User.findById(req.session.userId);
